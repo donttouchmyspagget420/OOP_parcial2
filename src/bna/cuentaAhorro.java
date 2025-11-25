35ca0794fce015585a482ba8541d1bbbe47f783e
sudoLucas <lucasgabrielramirez2004@gmail.com>
2025-11-25 01:55:49 -0300

    agregando cuenta de ahorro
    Implement cuentaAhorro class for savings account with interest calculation.

---
+++ src/bna/cuentaAhorro.java
@@ -0,0 +1,24 @@
+package bna;
+
+public class cuentaAhorro extends Cuenta {
+    private double tasaInteres;
+
+    public cuentaAhorro(int size, String nombre, int pin, double dinero, double tasaInteres) throws Exception {
+        super(size, nombre, pin, dinero);  
+        this.tasaInteres = tasaInteres;
+    }
+
+    public double calcularInteresMensual() {
+        return getDinero() * tasaInteres / 100;
+    }
+
+    public void aplicarInteresMensual() throws Exception {
+        double interes = calcularInteresMensual();
+        setDinero(getDinero() + interes);
+        agregarHistoria("Interés mensual: +" + interes);
+    }
+
+    public String mostrarBeneficios() {
+        return "Tasa interés: " + tasaInteres + "%\nInterés mensual: " + calcularInteresMensual();
+    }
+}
